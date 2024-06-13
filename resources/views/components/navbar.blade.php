50bf42a761f5a5b8f81fbca51613650aca9e1a72
Restu Lintangg <restulintang51@gmail.com>
2024-06-13 18:49:26 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <div class="header-right">
+            WELCOME TO RESTU'S<span style="color: white"> CURRICULUM VITAE</span>
+        </div>
+    </header>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <x-navbar></x-navbar>
+<main>
+        <div class="boxbesar">
+            <div class="box1">
+                <img src="../image/bersamabapa.jpg.jpeg" width="200px">
+                <h1>ABOUT ME</h1>
+                <p> Restu lintang Asih <br> Bogor, 03 Januari 2008 </p>        
+            </div>
+            <div class="box2">
+                <h3>Hallo, perkenalkan nama saya Restu Lintang Asih. Saya lahir di Bogor, 03 Januari 2008. <br> Sekarang saya berumur 15 tahun.
+                    Saya tinggal di Bogor, Cibedug. Dan saya beragama Islam. <br> Saya sedang menempuh pendidikan di SMK Wikrama Bogor.
+                    Hobi saya menulis.
+                </h3>
+                <h1> RIWAYAT PENDIDIKAN</h1>
+                    <ul>
+                        <li>SD: SDN BANJARSARI 02 (2014-2020)</li>
+                        <li>SMP: SMPN 1 CIAWI (2020-2023)</li>
+                        <li>SMK: SMK WIKRAMA BOGOR (2023-SEKARANG)</li>
+                    </ul>
+            </div>
+        </div>
+        <div class="boxbesar2">
+            <div class="kotak2">
+                <h1>PENGALAMAN</h1>
+                <ul>
+                    <li>Anggota Pramuka SDN Banjarsari 02</li>
+                    <li>Anggota Osis SMPN 1 Ciawi</li>
+                    <li>Anggota Paskibra SMPN 1 Ciawi</li>
+                </ul>
+            </div>
+            <div class="kotak1">
+                <h1>CONTACT</h1>
+                <ul>
+                    <li>WHATSAPP : +62882 2397 9172</li>
+                    <li>INSTAGRAM : _restulintang</li>
+                    <li>EMAIL : restulintang51@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <div class="boxbesar3">
+            <h1>PENGHARGAAN</h1>
+            <img src="../image/dicoding.png" width="200px">
+            <h3>Ini adalah sertifikat yang diberikan kepada saya karena telah mengikuti kelas BELAJAR DASAR PEMROGRAMAN WEB dan saya sudah berhasil menyelesaikan projek akhirnya, yaitu: membuat portofolio dengan menggunakan bahasa pemrograman HTML dan CSS. Yang diselenggarakan oleh Dicoding Academy.
+        </h3>
+        </div>
+    </main>
+<x-footer></x-footer>
+</body>
+</html>
